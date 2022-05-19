583bbda79cd2e5e959bcafb9f477ac63acd4f6d5
Bobcat997 <bszczypka978@gmail.com>
2022-05-19 16:06:59 +0200

    03_Flow_Control

--- 03_Flow_Control/01_while.py
+++
@@ -1,9 +0,0 @@
-# Napisz program zmieniający skalę w stopniach Fahrenheita na naszą w Celcjuszach. Program powinen wykonać się w pętli od 0 do 200 stopni Fahrenheit, co 20 stopni.
-# C = 5/9 * (F - 32) # wzór Celsjusz → Fahrenheit
-# Napisz rozwiązanie zarówno z użyciem pętli while jak i for.
-
-fahr = 0
-while fahr <= 200:
-    celc = 5/9 * (fahr - 32)
-    print(f'{fahr} st F -> {round(celc, 1)} st C')
-    fahr += 20
