c442326bdab4234c335df72af9dc51a08d2e26b2
Bobcat997 <98061337+Bobcat997@users.noreply.github.com>
2022-05-19 16:04:23 +0200

    Delete Homework/Flow_Control directory

--- Homework/Flow_Control/for_04.py
+++
@@ -1,9 +0,0 @@
-# Napisz program, który wyświetli kolejne wyniki dla silni liczby naturalnej N (N podane przez użytkownika, ale nie większe niż 8).
-number = int(input('Podaj liczbe od 1-8-->'))
-if number < 9:
-    silnia = 1
-    for index in range(2, number+1):
-        silnia = silnia * index
-    print(silnia)
-else:
-    print("Nieprawidłowa liczba")
