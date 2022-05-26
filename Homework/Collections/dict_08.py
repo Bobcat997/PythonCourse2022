3480ed14e73f6a6563b3ec172b488d586d0a607c
Bobcat997 <bszczypka978@gmail.com>
2022-05-26 19:58:41 +0200

    update

--- Homework/Collections/dict_08.py
+++ Homework/Collections/dict_08.py
@@ -1,3 +1,6 @@
+list1 = []
+l1 = []
+rev_names = {}
 names = {
 "Australia" : ["Olivia", "Charlotte", "Emily", "Chloe", "Ella", "Jessica", "Isabella", "Sophie", "Mia", "Emma"],
 "Belgium" : ["Emma", "Marie", "Laura", "Julie", "Sarah", "Clara", "Manon", "Léa", "Lisa", "Camille"],
@@ -9,3 +12,14 @@ names = {
 "Spain" : ["Lucía", "Sofia", "Martina", "Maria", "Paula", "Julia", "Emma", "Valeria", "Daniela", "Alba"],
 "Sweden" : ["Emma", "Maja", "Julia", "Alice", "Ida", "Linnéa", "Elin", "Alva", "Hanna", "Ella"],
 "United States" : ["Emily", "Emma", "Madison", "Abigail", "Olivia", "Isabella", "Hannah", "Samantha", "Ava", "Ashley"],
+}
+d1 =
+for i in names.values():
+    list1.extend(i)
+for i, v in enumerate(list1):
+    if int(list1.count(v)) >= 3:
+        l1.append(v)
+rev_names = set(l1)
+l1 = list(rev_names)
+a, b, c = l1
+print("imiona, które wystąpiły conajmniej w 3 krajach:", a, b ,c)
